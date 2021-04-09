99db9ea397ec2ff2c16ee80c70ede8db4c7e8205
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-09 23:47:33 +0200

    Added data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        String[][] data = new String[50][3];
+        String[][] data = {
+                {"Edilskrup", "Maribo", "28"},
+                {"Edilskrup", "Nykoebnig", "13"},
+                {"Edilskrup", "Vordingborg", "24"},
+                {"Haslev", "Korsoer", "60"},
+                {"Haslev", "Koege", "24"},
+                {"Haslev", "Naestved", "25"},
+                {"Haslev", "Ringsted", "19"},
+                {"Haslev", "Roskilde", "47"},
+                {"Haslev", "Slagelse", "48"},
+                {"Haslev", "Soroe", "34"},
+                {"Haslev", "Vordingborg", "40"},
+                {"Holbaek", "Jaergerspris", "34"},
+                {"Holbaek", "Kalundborg", "44"},
+                {"Holbaek", "Korsoer", "66"},
+                {"Holbaek", "Ringsted", "36"},
+                {"Holbaek", "Roskilde", "32"},
+                {"Holbaek", "Slagelse", "46"},
+                {"Holbaek", "Soroe", "34"},
+                {"Jaergerpris", "Korsoer", "95"},
+                {"Jaergerpris", "Koege", "58"},
+                {"Jaergerpris", "Ringsted", "56"},
+                {"Jaergerpris", "Roskilde", "33"},
+                {"Jaergerpris", "Slagelse", "39"},
+                {"Jaergerpris", "Soroe", "51"},
+                {"Kalundborg", "Ringsted", "56"},
+                {"Kalundborg", "Roskilde", "70"},
+                {"Klaundborg", "Slagelse", "39"},
+                {"Klaundborg", "Soroe", "51"},
+                {"Korsoer", "Naestved", "45"},
+                {"Korsoer", "Slagelse", "20"},
+                {"Koege", "Naestved", "45"},
+                {"Koege", "Ringsted", "29"},
+                {"Koege", "Roskilde", "25"},
+                {"Koege", "Vodringborg", "60"},
+                {"Maribo", "Nakskov", "27"},
+                {"Maribo", "Nykboeling F", "26"},
+                {"Naestved", "Roskilde", "57"},
+                {"Naestved", "Slagelse", "37"},
+                {"Naestved", "Soroe", "32"},
+                {"Naestved", "Vordingborg", "28"},
+                {"Ringsted", "Roskilde", "31"},
+                {"Ringsted", "Soroe", "15"},
+                {"Ringsted", "Vordingborg", "58"},
+                {"Slagelse", "Soroe", "14"}
+        };
 
         Graph g = new Graph(data);
 
