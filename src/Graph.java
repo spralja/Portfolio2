a497cfacefc2b0900113441794f18c8394c9c7de
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-11 13:22:31 +0200

    Update Graph.java
    Fixed constructor again
    Added prims algorithm to minimumSpanningTreeTotalWeight method, which return the total weight of the minimum spanning tree of a Graph g

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,6 @@
 import javax.print.DocFlavor;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.function.BinaryOperator;
 
 public class Graph {
     private HashMap<String, Vertex> vertices = new HashMap<>();
@@ -14,16 +13,26 @@ public class Graph {
         }
 
         public void addEdge(Vertex that, int weight) {
-            adjacent.add(that);
-            weights.add(weight);
+            this.adjacent.add(that);
+            this.weights.add(weight);
+            that.adjacent.add(this);
+            that.weights.add(weight);
+        }
+
+        public String getKey() {
+            return this.key;
         }
     }
 
     public Graph(String[][] data) {
         for(int i = 0; i < data.length; ++i) {
+            if(data[i][0] == null || data[i][1] == null || data[i][2] == null) continue;
             getVertex(data[i][0]).addEdge(getVertex(data[i][1]), Integer.parseInt(data[i][2]));
         }
     }
+
+    public Graph() {}
+
 
     public Vertex getVertex(String vertex) {
         if(vertices.get(vertex) == null) {
@@ -38,4 +47,52 @@ public class Graph {
         return this.getVertex(vertex);
     }
 
+    public static int minimumSpanningTreeTotalWeight(Graph g) {
+        Object[] vertices = g.vertices.keySet().toArray();
+        String[] keys = new String[vertices.length];
+        for (int i = 0; i < keys.length; ++i) {
+            keys[i] = (String) vertices[i];
+        }
+
+        String curr = keys[0];
+        PriorityQueue pq = new MinHeap();
+        HashMap<String, Integer> dist = new HashMap<>();
+        for (int i = 0; i < keys.length; ++i) {
+            dist.put(keys[i], Integer.MAX_VALUE);
+            pq.insert(keys[i], dist.get(keys[i]));
+        }
+
+        HashMap<String, String> parent = new HashMap<>();
+        HashMap<String, Boolean> explored = new HashMap<>();
+        for(int i = 0 ; i < keys.length; ++i) {
+            explored.put(keys[i], false);
+        }
+
+        dist.put(curr, 0);
+        pq.update(curr, dist.get(curr));
+        while (!pq.isEmpty()) {
+            curr = pq.removeMin().getKey();
+            explored.put(curr, true);
+            //System.out.println(curr + " -> " + parent.get(curr) + " = " + dist.get(curr));
+            for (int i = 0; i < g.vertices.get(curr).adjacent.size(); ++i) {
+                if(explored.get(g.vertices.get(curr).adjacent.get(i).getKey())) continue;
+                if (g.vertices.get(curr).weights.get(i) >= dist.get(g.vertices.get(curr).adjacent.get(i).getKey()))
+                    continue;
+
+                dist.put(g.vertices.get(curr).adjacent.get(i).getKey(), g.vertices.get(curr).weights.get(i));
+                parent.put(g.vertices.get(curr).adjacent.get(i).getKey(), curr);
+                pq.update(
+                        g.vertices.get(curr).adjacent.get(i).getKey(),
+                        dist.get(g.vertices.get(curr).adjacent.get(i).getKey())
+                );
+            }
+        }
+
+        int r = 0;
+        for(int i = 0; i < keys.length; ++i) {
+            r += dist.get(keys[i]);
+            System.out.println(keys[i] + " " + dist.get(keys[i]) + " " + parent.get(keys[i]));
+        }
+        return r;
+    }
 }
