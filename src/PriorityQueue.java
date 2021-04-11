fc9ee752394221ae54f0ec3b671e01f78202cb1d
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-11 13:21:23 +0200

    Update PriorityQueue.java
    Removed redundant 'public'
    Added updated method
    Added JavaDoc comments

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -1,8 +1,40 @@
 public interface PriorityQueue {
-    public void insert(String key, int value);
-    public Entry min();
-    public Entry removeMin();
-    public int size();
+    /**
+     * Insert a key, value pair into the heap
+     * @param key associated with the value
+     * @param value sorted by
+     */
+    void insert(String key, int value);
+
+    /**
+     * Get the entry with the smallest value
+     * @return the entry with the smallest value
+     */
+    Entry min();
+
+    /**
+     * Remove the entry with the smallest value
+     * @return the smallest value
+     */
+    Entry removeMin();
+
+    /**
+     * Get the number of entries in the heap
+     * @return the number of entries in the heap
+     */
+    int size();
+
+    /**
+     * Update the value of a key in the heap
+     * @param key the key whose value is to be updated
+     * @param value the new value
+     */
+    void update(String key, int value);
+
+    /**
+     * Tests whether the heap is empty
+     * @return true if empty otherwise false
+     */
     default boolean isEmpty() {
         return (size() == 0);
     }
