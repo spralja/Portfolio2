f0f8e88435994a2857be9be58d1371db243afbb2
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-04-28 10:33:29 +0200

    Fluff

--- src/Entry.java
+++ src/Entry.java
@@ -1,17 +1,12 @@
 public class Entry {
-    private String key = null;
-    private int value = Integer.MAX_VALUE;
+    private final String key;
+    private final int value;
 
     public Entry(String key, int value) {
         this.key = key;
         this.value = value;
     }
 
-    public String getKey() {
-        return this.key;
-    }
-
-    public int getValue() {
-        return this.value;
-    }
+    public String getKey() { return this.key; }
+    public int getValue() { return this.value; }
 }
